004181cc59332291f8b6836001bedb5d9d07baff
Kavindu Prabhashitha <donprabhashitha@gmail.com>
2023-02-02 20:26:49 +0530

    project  - search - fixed

--- ovros_home/forms.py
+++ ovros_home/forms.py
@@ -1,17 +1,17 @@
 from django import forms
-from common.enums import VEHICLE_TYPE, DISTRICTS
+from common.enums import VEHICLE_TYPE, DISTRICTS, DISTRICTS_CITY
 
 
 class ServiceSearchForm(forms.Form):
-    search_by_district = forms.ChoiceField(choices=DISTRICTS, required=False)
-    search_by_city = forms.ChoiceField(required=False)
+    search_by_district = forms.ChoiceField(required=False, choices=DISTRICTS, initial=DISTRICTS[1])
+    search_by_city = forms.ChoiceField(required=False, choices=DISTRICTS_CITY)
     search_by_vehicle = forms.ChoiceField(required=False, choices=VEHICLE_TYPE, initial=VEHICLE_TYPE[0])
     search_by_Keyword = forms.CharField(required=False, max_length=50)
 
 
 class ServiceSearchForm01(forms.Form):
-    search_by_district = forms.ChoiceField(choices=DISTRICTS, required=False)
-    search_by_city = forms.ChoiceField(required=False, help_text="first select a district")
+    search_by_district = forms.ChoiceField(choices=DISTRICTS, required=False, initial=DISTRICTS[1])
+    search_by_city = forms.ChoiceField(required=False, help_text="first select a district", choices=DISTRICTS_CITY)
 
 
 class ServiceSearchForm02(forms.Form):
